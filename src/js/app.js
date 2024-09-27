d346444c07bc4b1511bfa9735c3c8d0e5e1619de
YarikSintsov <sintsovyarik@yandex.ru>
2024-09-27 16:19:34 +0300

    all commit

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,6 @@ import {tabs} from './includes/functions/tabs.js'
 import {spollers} from './includes/functions/spollers.js'
 import * as scroll from './includes/functions/scroll/scroll.js'
 import * as forms from './includes/functions/forms/forms.js'
-import { off } from 'gulp'
 
 let menu = document.getElementById('menu-btn')
 let html = document.getElementById('html')
@@ -14,15 +13,37 @@ let html = document.getElementById('html')
 menu.onclick = function(){
     html.classList.toggle('menu-open')
 }
-let wrapp1 = document.getElementById('w1')
-let wrapp2 = document.getElementById('w2')
-let li1 = document.getElementById('h')
-let li2 = document.getElementById('b')
-li1.onclick = function(){
-    wrapp2.classList.add('none')
-    wrapp1.classList.remove('none')
+let wrappers = document.querySelectorAll('.wrapper')
+let links = document.querySelectorAll('.link')
+links[0].onclick = function(){
+    wrappers[0].classList.remove('none')
+    wrappers[1].classList.add('none')
+    wrappers[2].classList.add('none')
+    wrappers[3].classList.add('none')
+    wrappers[4].classList.add('none')
+    wrappers[5].classList.add('none')
 }
-li2.onclick = function(){
-    wrapp1.classList.add('none')
-    wrapp2.classList.remove('none')
+links[1].onclick = function(){
+    wrappers[1].classList.remove('none')
+    wrappers[0].classList.add('none')
+    wrappers[2].classList.add('none')
+    wrappers[3].classList.add('none')
+    wrappers[4].classList.add('none')
+    wrappers[5].classList.add('none')
+}
+links[2].onclick = function(){
+    wrappers[2].classList.remove('none')
+    wrappers[0].classList.add('none')
+    wrappers[1].classList.add('none')
+    wrappers[3].classList.add('none')
+    wrappers[4].classList.add('none')
+    wrappers[5].classList.add('none')
+}
+links[3].onclick = function(){
+    wrappers[3].classList.remove('none')
+    wrappers[0].classList.add('none')
+    wrappers[1].classList.add('none')
+    wrappers[2].classList.add('none')
+    wrappers[4].classList.add('none')
+    wrappers[5].classList.add('none')
 }
